e8777bf40f662bf3aae971671717c06431ef001b
Hossein Roozdar <hoesin122466@gmail.com>
2024-01-03 02:15:34 -0800

    Add connect and disconnect sevent handler

--- server/server.js
+++ server/server.js
@@ -1,21 +1,23 @@
-const path=require('path')
-const express=require('express');
-var publicPath= path.join(__dirname, '../public')
+const path = require("path");
+const express = require("express");
+const socketIO = require("socket.io");
+const http = require("http");
+var publicPath = path.join(__dirname, "../public");
 
 const app = express();
-const port=process.env.PORT || 3000;
-app.use(express.static(publicPath))
+const server = http.createServer(app);
+var io = socketIO(server);
 
+io.on("connection", (socket) => {
+  console.log("new User connction");
+  socket.on('disconnected',()=>{
+    console.log('userwas disconnected')
+  })
+});
 
-app.listen(port,()=>{
-    console.log(`server is up on ${port }`)
-})
-app.listen(port,()=>{
-    console.log(`server is up on ${port }`)
-})
-app.listen(port,()=>{
-    console.log(`server is up on ${port }`)
-})
-app.listen(port,()=>{
-    console.log(`server is up on ${port }`)
-})
+const port = process.env.PORT || 3000;
+app.use(express.static(publicPath));
+
+server.listen(port, () => {
+  console.log(`server is up on ${port}`);
+});
